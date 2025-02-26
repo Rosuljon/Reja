8f45a558136e20cf82587d01a688381bf221a7b5
Rosuljon <ikramjanov97@mail.ru>
2025-02-26 09:39:00 +0900

    fix: refactoring train.js file

--- train.js
+++ train.js
@@ -1,75 +1,16 @@
-// console.log("Jack Ma maslahatlari");
+// ********************************   Task B     *****************************************
 
-const list = [
-  "yaxshi talaba buling", //0-20
-  "togri boshliq tanlang va koproq hato qiling", //20-30
-  "uzingizni ishlaringizni boshlang", //30-40
-  "siz kuchli bolgan narsalarni qiling", //40-50
-  "yoshlarga investitsiya qiling", //50-60
-  "endi dam oling, foydasi yoq", //60~
-];
+function countDigit(word) {
+  let count = 0;
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] >= "0" && word[i] <= "9") {
+      count += 1;
+    }
+  }
+  return count;
+}
 
-// ********************************   Callback function      *****************************************
-
-// function maslahatBering(a, callback) {
-//   if (typeof a !== "number") callback("insert a number", null);
-//   else if (a <= 20) callback(null, list[0]);
-//   else if (a <= 30) callback(null, list[1]);
-//   else if (a <= 40) callback(null, list[2]);
-//   else if (a <= 50) callback(null, list[3]);
-//   else if (a <= 60) callback(null, list[4]);
-//   else {
-//     setInterval(function () {
-//       callback(null, list[5]);
-//     }, 1000);
-//   }
-// }
-
-// console.log("passed here 0");
-// maslahatBering(65, (err, data) => {
-//   if (err) console.log("ERROR:", err);
-//   else console.log("javob:", data);
-// });
-// console.log("passed here 1");
-
-// ********************************   Async function, then(), catch(), Promise     *****************************************
-
-// async function maslahatBering(a, callback) {
-//   if (typeof a !== "number") throw new Error("Enter a number");
-//   else if (a <= 20) return list[0];
-//   else if (a <= 30) return list[1];
-//   else if (a <= 40) return list[2];
-//   else if (a <= 50) return list[3];
-//   else if (a <= 60) return list[4];
-//   else {
-//     return new Promise((resolve, reject) => {
-//       setTimeout(() => {
-//         resolve(list[5]);
-//       }, 5000);
-//     });
-//   }
-// }
-
-// console.log("passed here 0");
-// maslahatBering(25)
-//   .then((data) => {
-//     console.log("javob:", data);
-//   })
-//   .catch((err) => {
-//     console.log("ERROR:", err);
-//   });
-// console.log("passed here 1");
-
-// ********************************   Async function, await     *****************************************
-// async function run() {
-//   let javob = await maslahatBering(25);
-//   console.log(javob);
-//   javob = await maslahatBering(65);
-//   console.log(javob);
-//   javob = await maslahatBering(45);
-//   console.log(javob);
-// }
-// run();
+console.log(countDigit("hell212otheewfew23123re"));
 
 // ********************************   Task A     *****************************************
 // Bu code:
@@ -91,17 +32,3 @@ const list = [
 
 // const result = countLetter("s", "Assessment");
 // console.log(result);
-
-// ********************************   Task B     *****************************************
-
-function countDigit(word) {
-  let count = 0;
-  for (let i = 0; i < word.length; i++) {
-    if (word[i] >= "0" && word[i] <= "9") {
-      count += 1;
-    }
-  }
-  return count;
-}
-
-console.log(countDigit("hell212otheewfew23123re"));
