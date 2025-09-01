b3f104bdcbba9d2ee0038c076f14ad6a70e1b581
Rosuljon <ikramjanov97@mail.ru>
2025-09-01 15:39:08 +0900

    feat: create Dockerfile to generate images

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ mongodb.connect(connectString, {}, (err, client) => {
 
     const app = require("./app");
     const server = http.createServer(app);
-    let PORT = 4007;
+    let PORT = 3000;
     server.listen(PORT, function () {
       console.log(`Server running at http://localhost:${PORT}`);
     });
